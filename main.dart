756771738371e9127831775c93a5b719fa9e369b
baskom123 <116578096+baskom123@users.noreply.github.com>
2023-10-23 18:21:49 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/m03/Screen.dart';
+import 'package:provider/provider.dart';
+import 'm03/MyProvider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ListProductProvider(),
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const Screen()),
+    );
+  }
+}
